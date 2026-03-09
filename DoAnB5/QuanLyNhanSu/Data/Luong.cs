4c2291b65dc760b1369f33bd8e107f1740054abf
HuynhNhu0104 <nhu_dth235723@student.agu.edu.vn>
2026-03-09 21:34:24 +0700

    Da em nop bai a

---
+++ DoAnB5/QuanLyNhanSu/Data/Luong.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu.Data
+{
+    public class Luong
+    {
+        [Key]
+        public int Id { get; set; }
+        public double LuongCoBan { get; set; }
+        public double ThucLanh { get; set; }
+
+        public int NhanVienId { get; set; }
+        public virtual NhanVien NhanVien { get; set; }
+    }
+}
---
+++ DoAnB5/QuanLyNhanSu/Data/TaiKhoan.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class TaiKhoan
+{
+    [Key]
+    public string TenDangNhap { get; set; }
+    [Required]
+    public string MatKhau { get; set; }
+    public string Quyen { get; set; } // Admin hoặc User
+}
